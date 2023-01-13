01d728b9920d98dd07609d2bd2e351527811bd45
Risfa <risfandhiani@gmail.com>
2023-01-14 00:03:37 +0700

    first commit

---
+++ src/main/java/BidangDatar.java
@@ -0,0 +1,42 @@
+/**
+ *
+ * @author Risvandiani
+ */
+public class BidangDatar {
+    double alas, tinggi;
+    int sisi, panjang, lebar;
+    float diameter;
+    
+    public double isiSegitigaTrapesium (double a, double t){
+       this.alas = a; 
+       this.tinggi = t;
+       double luas = 0.5*a*t;
+       return luas;
+    }
+    public void isiPersegi (int s) {
+        this.sisi = s;
+        int luas = s*s;
+    }
+    public void isiPersegiPanjang (int p, int l){
+        this.panjang = p;
+        this.lebar = l;
+    }
+    public float isiLingkaran (float d) {
+       this.diameter = d;
+       float luas = d;
+       return luas;
+    }
+    
+    void luasSegitiga () {
+        System.out.println ();
+    }
+    public int luasPersegi () {
+        return sisi*sisi;
+    }
+    public int luasPersegiPanjang (){
+        return panjang*lebar;
+    }
+    public float luasLingkaran (){
+        return (float) (Math.PI*diameter);
+    }
+}
---
+++ src/main/java/UjiBangunDatar.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Risvandiani
+ */
+public class UjiBangunDatar {
+
+    public static void main(String[] args) {
+
+        BidangDatar BD = new BidangDatar ();
+        BD.isiSegitigaTrapesium(8, 3);
+        BD.isiPersegi(10);
+        BD.isiPersegiPanjang(5, 5);
+        BD.isiLingkaran(7);
+        }
+    
+}
